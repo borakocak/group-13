bc0bd78b157ab794eab21c73ada76d964a013773
Maheli07 <Maheli07@github.com>
2021-05-27 22:45:00 +0200

    Added the Firebase connection, Register methods and updated the Login view

--- JoystickTest/app/src/main/java/com/example/joysticktest/MainActivity2.java
+++ JoystickTest/app/src/main/java/com/example/joysticktest/MainActivity2.java
@@ -1,35 +1,118 @@
 package com.example.joysticktest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
+    EditText mVehicleIdNumber, mEmail, mPassword;
+    Button mLoginBtn;
+    Button mRegisterBtn;
+    FirebaseAuth fAuth;  //instance of the class
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
 
-        Button login = (Button)findViewById(R.id.login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            /*Button login = (Button) findViewById(R.id.login);
 
-                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
-                //ComponentName componentName = new ComponentName("com.example.joysticktest","com.example.joysticktest.MainActivity");
-                //intent.setComponent(componentName);
-                startActivity(intent);
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    //ComponentName componentName = new ComponentName("com.example.joysticktest","com.example.joysticktest.MainActivity");
+                    //intent.setComponent(componentName);
+                    startActivity(intent);
+                }
+            });*/
+
+            mVehicleIdNumber = findViewById(R.id.vehicleIdNumber);
+            mEmail = findViewById(R.id.email);
+            mPassword = findViewById(R.id.password);
+            mLoginBtn = findViewById(R.id.loginBtn);
+            mRegisterBtn = findViewById(R.id.registerBtn);
+            fAuth = FirebaseAuth.getInstance();
+
+            if (fAuth.getCurrentUser() != null) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
-        });
+
+            mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String vehicleIdNumber = mVehicleIdNumber.getText().toString().trim();
+                    String email = mEmail.getText().toString().trim();
+                    String password = mPassword.getText().toString().trim();
+
+                   /* Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    //ComponentName componentName = new ComponentName("com.example.joysticktest","com.example.joysticktest.MainActivity");
+                    //intent.setComponent(componentName);
+                    startActivity(intent);*/
+
+                    if (TextUtils.isEmpty(vehicleIdNumber)) {
+                        mVehicleIdNumber.setError("Vehicle ID number is required.");
+                        return;
+                    }
+                    if (TextUtils.isEmpty(email)) {
+                        mEmail.setError("Name is required.");
+                        return;
+                    }
+                    if (TextUtils.isEmpty(password)) {
+                        mPassword.setError("Password is required.");
+                        return;
+                    }
+                    if (password.length() < 6) {
+                        mPassword.setError("Password must contain atleast 6 Characters");
+                        return;
+                    }
 
 
+                }
+            });
+            mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+/*                    String email = mEmail.getText().toString().trim();
+                    String password = mPassword.getText().toString().trim();
+                    startActivity(new Intent(getApplicationContext(), RegisterUser.class));
 
+                    //register the user in the firebase
+
+                    fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity2.this, "User Logged in.", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(MainActivity2.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });*/
+                    startActivity(new Intent(getApplicationContext(), RegisterUser.class));
+
+
+                }
+
+            });
+        }
     }
-}
--- JoystickTest/app/src/main/java/com/example/joysticktest/RegisterUser.java
+++ JoystickTest/app/src/main/java/com/example/joysticktest/RegisterUser.java
@@ -1,13 +1,125 @@
 package com.example.joysticktest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterUser extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    EditText mVehicleIdNumber, mName, mEmail, mPassword ,mConfirmPassword;
+    Button mLoginBtn;
+    Button mRegisterBtn;
+    FirebaseAuth fAuth;  //instance of the class
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mVehicleIdNumber = findViewById(R.id.vehicleIdNumber);
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mConfirmPassword = findViewById(R.id.confirmPassword);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String vehicleIdNumber = mVehicleIdNumber.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String confirmPassword = mConfirmPassword.getText().toString().trim();
+                String name = mName.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(vehicleIdNumber)) {
+                    mVehicleIdNumber.setError("Vehicle ID number is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Name is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password must contain at least 6 Characters");
+                    return;
+                }
+
+                //authenticate the user
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterUser.this, "User Registered Successfully.", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("vehicleIDNumber",vehicleIdNumber);
+                            user.put("name",name);
+                            user.put("email",email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void avoid) {
+                                    Log.d(TAG, "onSuccess: user profile is created for" +userID);
+
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(RegisterUser.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+
+            }
+        });
     }
 }
